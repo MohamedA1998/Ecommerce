cbd8bc211d49aa66bedb68b8915fe4030d7d0398
Mohamed Alsayed <malsayed493@gmail.com>
2025-06-15 22:33:17 +0300

    handel category model

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\Category::factory()->count(10)->create()->each(function ($category) {
+                $category->image()->create([
+                    'image' => 'https://picsum.photos/seed/' . $category->id . '/300/200',
+                ]);
+            });
+    }
+}
